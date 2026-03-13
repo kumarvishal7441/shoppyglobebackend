fc60fe969bf02f6845fc669763a0ce2a8249e46a
kumarvishal7441 <kumarvishal7441@gmail.com>
2026-03-13 21:35:14 +0530

    create middleware for authorization

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,54 +1,58 @@
 import userModal from "../modal/user.modal.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 // create user
-export async function Register(req,res){
-    try{
-        const{user,email,password}=req.body
+export async function Register(req, res) {
+    try {
+        const { user, email, password } = req.body
 
-    const data = await userModal.findOne({email});
-    if(data){
-       return res.status(409).json("user already exit")
-    } else {
-        const newuser =  await userModal.create(
-            {
-                user,
-                email,
-                password:bcrypt.hashSync(password, 10),
-            }
-        );
-        console.log(newuser)
-        return res.status(201).json({"new user successfully created":newuser})
+        const data = await userModal.findOne({ email });
+        if (data) {
+            return res.status(409).json("user already exit")
+        } else {
+            const newuser = await userModal.create(
+                {
+                    user,
+                    email,
+                    password: bcrypt.hashSync(password, 10),
+                }
+            );
+            console.log(newuser)
+            return res.status(201).json({ "new user successfully created": newuser })
+        }
+    } catch (error) {
+        res.status(500).json({ "err": error.message })
     }
-    } catch (error){
-        res.status(500).json({"err":error.message})
-    }
-    
+
 }
 
 // verify user
-export async function Login(req,res){
-        try{
-        const{email,password}=req.body
+export async function Login(req, res) {
+    try {
+        const { email, password } = req.body
 
-    const data = await userModal.findOne({email});
-    if(!data){
-       return res.status(409).json("user not found")
-    } else {
-        let validpass = bcrypt.compareSync(password,data.password)
-        if(!validpass){
-            res.status(409).json("user password wrong")
-        } else{
-            return res.status(200).json({user:{
-                email:data.email,
-                user:data.user
+        const data = await userModal.findOne({ email });
+        if (!data) {
+            return res.status(409).json("user not found")
+        } else {
+            let validpass = bcrypt.compareSync(password, data.password)
+            if (!validpass) {
+                res.status(409).json("user password wrong")
             }
-            
-        })
+            var token = jwt.sign({ id: data.id }, 'secret',{expiresIn:"30m"});
+            return res.status(200).json({
+                user: {
+                    email: data.email,
+                    user: data.user
+                },
+                accessToken :token,
+
+            })
+
+            return res.status(201).json({ "new user successfully created": newuser })
         }
-        return res.status(201).json({"new user successfully created":newuser})
-    }
-    } catch (error){
-        res.status(500).json({"err":error.message})
+    } catch (error) {
+        res.status(500).json({ "err": error.message })
     }
 }
